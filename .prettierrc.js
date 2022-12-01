6b312b1a30e109a67dd3d4d0709b461548e4d5cb
Nicola Palavecino <nicolap@mural.co>
2022-12-01 18:35:00 -0300

    Update linter configuration for this repo

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
-module.exports =  {
-    semi:  true,
-    trailingComma:  'all',
-    singleQuote:  true,
-    printWidth:  80,
-    tabWidth:  2,
-  };
+module.exports = {
+  semi:  true,
+  trailingComma:  'all',
+  singleQuote:  true,
+  printWidth:  80,
+  tabWidth:  2,
+};
   
